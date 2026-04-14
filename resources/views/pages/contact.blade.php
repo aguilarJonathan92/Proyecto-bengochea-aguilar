7ec0fd655102861efe00b328604be9624756c20b
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-04-14 09:33:03 -0300

    Refactor contact page layout with Blade components

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<x-layout>
+    <x-slot name="title">Contacto</x-slot>
+    <h2 class="text-center bg-white">Canales De Contacto</h2>
+</x-layout>
