dfd4a68be4a80770cd7263a1c97b02943569a030
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-04-14 09:26:08 -0300

    Update routes to new page views
    Updated routes to point to new view files for home, contact, catalog, marketing, queries, terms, and about pages.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
-});
+    return view('pages.home');
+})->name('home');
+
+Route::get('/contacto', function () {
+    return view('pages.contact');
+})->name('contact');
+
+Route::get('/catalogo', function () {
+    return view('pages.catalog');
+})->name('catalog');
+
+Route::get('/comercializacion', function () {
+    return view('pages.marketing');
+})->name('marketing');
+
+Route::get('/consultas', function () {
+    return view('pages.queries');
+})->name('queries');
+
+Route::get('/terminos', function () {
+    return view('pages.term-and-uses');
+})->name('terms');
+
+Route::get('/acerca-de', function () {
+    return view('pages.about');
+})->name('about');
