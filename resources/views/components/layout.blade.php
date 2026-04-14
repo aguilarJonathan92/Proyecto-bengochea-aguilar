bc389a96988cbea564144b13e55f4ffa03b93af1
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-04-14 09:28:52 -0300

    Initialize layout with HTML structure and assets

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Inicio'}}</title>
+    <link rel="stylesheet" href="{{ asset('vendor/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css')}}">
+</head>
+<body class="fondo">
+    <x-header/>
+    <x-navbar/>
+    {{ $slot }}
+    <script src=" {{ asset('vendor/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
