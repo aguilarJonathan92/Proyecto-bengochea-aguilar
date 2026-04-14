584ac2fc6197d8f5162333b93bd76443e2e12b49
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-04-14 09:29:43 -0300

    Refactor header layout and update logo link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,33 +2,33 @@
     <div class="row align-items-center">
 
         {{-- logo --}}
-        <div class="col-md-3 d-flex justify-content-center fs-3">
-            <a class="navbar-brand texto-rojo" href="#">SOUNDWAVE<span class="text-secondary"> STORE</span></a>
+        <div class="col-xl-3 col-sm-12 d-flex justify-content-center fs-3">
+            <a class="navbar-brand texto-rojo" href="{{ route('home') }}">SOUNDWAVE<span class="text-secondary"> STORE</span></a>
         </div>
-        
+
         {{-- buscador --}}
-        <div class="col-md-6">
+        <div class="col-xl-6 p-2 px-xl-4 bd-search">
             <form class="d-flex">
                 <input class="form-control rounded-start-pill" type="search" placeholder="¿Qué estás buscando?">
                 <button class="btn btn-outline-secondary rounded-end-pill" type="submit">
-                    <img src="{{ asset('icons/svg/buscar.svg')}}" alt="buscar"> 
+                    <img src="{{ asset('icons/svg/buscar.svg')}}" alt="buscar">
                 </button>
             </form>
         </div>
-        
+
         {{-- carrito e inicio de sesion --}}
-        <div class="col-md-3 d-flex justify-content-center align-items-center">
+        <div class="col-xl-3 d-flex justify-content-center align-items-center">
             <div class="d-flex align-items-center me-3">
                 <a href="#" class="d-flex align-items-center text-decoration-none">
                     <img src="{{ asset('icons/svg/persona.svg')}}" alt="iniciar Sesion">
-                
+
                     <div class="d-flex flex-column" style="line-height: 1.2;">
                         <span class="fw-semibold text-white">¡Hola! Iniciá sesión</span>
                         <small class="text-secondary">O podés registrarte</small>
                     </div>
                 </a>
             </div>
-            
+
             <a href="#" class="text-decoration-none px-2"><img src="{{ asset('icons/svg/carrito.svg')}}" alt="carrito de compras"></a>
             <a href="#" class="text-decoration-none"><img src="{{ asset('icons/svg/modo-claro.svg')}}" alt="modo claro"></a>
         </div>
