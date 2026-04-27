596e4052232e9bf59d32dee0c5725e6fe18569f8
jonathan <aguilarsistemas92@gmail.com>
2026-04-27 10:00:57 -0300

    acomodo estilos en diferentes archivos css y doy estilo a consulta

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,8 +7,7 @@
     <link rel="icon" href="{{ asset('icons/ico/icono-soundWave.ico') }}">
     <title>{{ $title ?? 'Inicio' }}</title>
     <link rel="stylesheet" href="{{ asset('vendor/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
--- resources/views/pages/queries.blade.php
+++ resources/views/pages/queries.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <x-slot name="title">Consultas</x-slot>
     <div class="container p-2 p-md-4 px-lg-5">
-        <div class="pages-decoration text-light my-4 px-3 px-md-5 pb-4">
-            <h2 class="text-center border-bottom py-4">Formulario de consulta</h2>
+        <div class="pages-decoration text-light my-4 px-3 px-md-5 pb-4 card-contact" >
+            <h2 class="text-center border-bottom py-4 line-bottom">Formulario de consulta</h2>
             <p class="text-center mb-4">
                 Nuestro compromiso es responder cada consulta con la mayor eficiencia posible,
                 asegurando un servicio confiable y de calidad.
@@ -12,18 +12,18 @@
                 <div class="row">
                     <div class="col-12 col-md-6 mb-3">
                         <label for="nombre" class="form-label">Nombre *</label>
-                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Jonathan" required>
+                        <input type="text" class="form-control checkout-input" id="nombre" name="nombre" placeholder="Jonathan" required>
                     </div>
                     <div class="col-12 col-md-6 mb-3">
                         <label for="email" class="form-label">Email *</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="cuenta@correo.com"
+                        <input type="email" class="form-control checkout-input" id="email" name="email" placeholder="cuenta@correo.com"
                             required>
                     </div>
                 </div>
 
                 <div class="mb-3">
                     <label for="asunto" class="form-label">Asunto *</label>
-                    <select id="asunto" name="asunto" class="form-select" aria-label="Seleccionar asunto" required>
+                    <select id="asunto" name="asunto" class="form-select checkout-input" aria-label="Seleccionar asunto" required>
                         <option value="" selected disabled>Elija una opción</option>
                         <option value="1">Formas de pago</option>
                         <option value="2">Modos/costos de envío</option>
@@ -35,11 +35,11 @@
 
                 <div class="mb-3">
                     <label for="mensaje" class="form-label">Mensaje *</label>
-                    <textarea class="form-control" id="mensaje" name="mensaje" rows="4" placeholder="Escribe tu consulta aquí..." required></textarea>
+                    <textarea class="form-control checkout-input" id="mensaje" name="mensaje" rows="4" placeholder="Escribe tu consulta aquí..." required></textarea>
                 </div>
 
                 <div class="d-grid d-md-block text-center">
-                    <button type="submit" class="btn btn-brand px-5 py-2">Enviar Mensaje</button>
+                    <button type="submit" class="btn-enviar">Enviar Mensaje</button>
                 </div>
             </form>
         </div>
