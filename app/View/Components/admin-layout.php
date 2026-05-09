dc03c154db768dc802620a4b03e2df082e2fdb89
jonathan <aguilarsistemas92@gmail.com>
2026-05-09 09:12:13 -0300

    creo el modelo Category con su migracion

--- app/View/Components/admin-layout.php
+++ app/View/Components/admin-layout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class admin-layout extends Component
+class admin_layout extends Component
 {
     /**
      * Create a new component instance.
