a3852c66090f4eca3279f800b744ecaa6bf943e1
Marcelo <bengmar7@gmail.com>
2026-06-01 02:42:06 -0300

    "Editar" por "Cambiar Estado"

--- app/Filament/Resources/Queries/Pages/ViewQuery.php
+++ app/Filament/Resources/Queries/Pages/ViewQuery.php
@@ -13,7 +13,8 @@ class ViewQuery extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            EditAction::make()
+            ->label('Cambiar Estado'),
         ];
     }
 }
