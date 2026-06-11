8524598527fe13a794884c379756d323fb4f0098
Marcelo <bengmar7@gmail.com>
2026-06-11 10:08:31 -0300

    Correcciones en vistas de 'errores'

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,7 +3,7 @@
 
     <div style="text-align: center; padding: 50px;">
         <h1>¡Ups! 🎸</h1>
-        <p>Parece que lo que buscas no está en nuestro sitio web</p>
+        <p>No tienes permiso para acceder a esta ruta</p>
         <img src="{{ asset('images/guitarra_error404.png') }}" alt="Error 404"
             style="width: 350px; border: 1px solid gray;">
         <br><br>
