0b96349a1c79b7315ecffd6561d5dd01a004fbda
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-06-15 17:38:12 -0300

    Agregados importaciones perdidas para UserResource

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -2,13 +2,13 @@
 
 namespace App\Filament\Resources\Users;
 
-use App\Filament\Resources\Products\Pages\ViewUser;
-use App\Filament\Resources\Queries\Schemas\UserInfolist;
 use App\Filament\Resources\Users\Pages\CreateUser;
 use App\Filament\Resources\Users\Pages\EditUser;
 use App\Filament\Resources\Users\Pages\ListUsers;
+use App\Filament\Resources\Users\Pages\ViewUser;
 use App\Filament\Resources\Users\RelationManagers\OrdersRelationManager;
 use App\Filament\Resources\Users\Schemas\UserForm;
+use App\Filament\Resources\Users\Schemas\UserInfolist;
 use App\Filament\Resources\Users\Tables\UsersTable;
 use App\Models\User;
 use BackedEnum;
