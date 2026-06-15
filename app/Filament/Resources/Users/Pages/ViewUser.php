adc1a2306d004cbcf26bc3c3f7175e79432e8898
Marcelo <99445898+bengmar@users.noreply.github.com>
2026-06-15 17:39:17 -0300

    Actualizado namespace para ViewUser.php

--- app/Filament/Resources/Users/Pages/ViewUser.php
+++ app/Filament/Resources/Users/Pages/ViewUser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\Products\Pages;
+namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\EditAction;
